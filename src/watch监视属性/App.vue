7095982e6ce641da43816f02d7a3379217a6b0bb
15960665735@163.com <yubinwang_b@163.com>
2025-07-26 21:37:16 +0800

    选项式API的 watch监视

---
+++ src/watch监视属性/App.vue
@@ -0,0 +1,47 @@
+<template>
+<h2>当前数据为{{sum}}</h2>
+<button @click="sum++">点我变换数据</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'App',
+  watch:{
+    // 简写
+      // sum(newValue,oldValue) {
+      //   console.log('sum值发生了改变',newValue, oldValue);
+      // }
+      sum:{
+        immediate:true,
+        // 深度监视
+        deep:true,
+        handler(newValue,oldValue) {
+           console.log('sum值发生了改变',newValue, oldValue);
+
+        }
+      }
+
+
+    },
+  setup(){
+    let sum = ref(0);
+
+    //返回一个对象
+    return {
+      sum
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
