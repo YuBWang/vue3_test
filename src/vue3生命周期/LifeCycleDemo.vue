48eb2a2b91487ef4bf37055fe16d66a9131152cf
15960665735@163.com <yubinwang_b@163.com>
2025-08-09 18:08:15 +0800

    vue3生命周期

---
+++ src/vue3生命周期/LifeCycleDemo.vue
@@ -0,0 +1,68 @@
+<template>
+    <h1>当前值为: {{sum}}</h1>
+    <button @click="sum++">点我值加1</button>
+
+</template>
+
+<script>
+import {ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from 'vue'
+export default {
+    name:'LifeCycleDemo',
+    setup(){
+       var sum = ref(0);
+
+       // 组合式API
+    onBeforeMount(()=>{
+      console.log("==================onBeforeMount")
+    })
+    onMounted(()=>{
+      console.log("==================onMounted")
+    })
+    onBeforeUpdate(()=>{
+      console.log("==================onBeforeUpdate")
+    })
+    onUpdated(()=>{
+      console.log("==================onUpdated")
+    })
+    onBeforeUnmount(()=>{
+      console.log("==================onBeforeUnmount")
+    })
+    onUnmounted(()=>{
+      console.log("==================onUnmounted")
+    })
+
+        return{
+            sum
+        }
+
+    },
+    // 通过配置项的形式，使用生命周期钩子
+    // beforeCreate(){
+    //     console.log("==================beforeCreate=================")
+    // },
+    // created(){
+    //     console.log("==================created=================")
+    // },
+    // beforeMount(){
+    //     console.log("==================beforeMount=================")
+    // },
+    // mounted(){
+    //     console.log("==================mounted=================")
+    // },
+    // beforeUpdate(){
+    //     console.log("==================beforeUpdate=================")
+    // },
+    // updated(){
+    //     console.log("==================updated=================")
+    // },
+    // beforeUnmount(){
+    //     console.log("==================beforeUnmount=================")
+    // },
+    // unmounted(){
+    //     console.log("==================unmounted=================")
+    // }
+
+
+}
+
+</script>
