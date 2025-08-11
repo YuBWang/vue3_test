5321f63fcd944c2a845eb579b76d7ebedd84bd64
15960665735@163.com <yubinwang_b@163.com>
2025-08-11 22:38:09 +0800

    toref的使用

---
+++ src/hooks/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <LifeCycleDemo></LifeCycleDemo>
+
+</template>
+
+<script>
+import { ref } from 'vue';
+import LifeCycleDemo from './自定义hook/LifeCycleDemo.vue';
+import pt from './hooks/爱心';
+export default {
+  name: 'App',
+  components:{
+    LifeCycleDemo
+  },
+  setup(){
+    let sum = ref(0);
+    pt();
+    //返回一个对象
+    return {
+      sum
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/toRefs/PersonUse.vue
@@ -0,0 +1,44 @@
+<template>
+
+<br>
+<h3>姓名：{{person.name}}</h3>
+<h3>年龄：{{person.age}}</h3>
+<h3>班级：{{person.school.cl}}</h3>
+<button @click="person.age++">点我修改年龄</button>
+<button @click="person.name=person.name+'~'">点我修改名称</button>
+<button @click="person.school.cl=person.school.cl+'班'">点我修改班级</button>
+</template>
+
+<script>
+import { reactive ,toRef} from 'vue';
+export default {
+  name: 'PersonUse',
+  setup(){
+    let person = reactive({
+      name:"张三",
+      age:18,
+      school:{
+        cl:"五班"
+      }
+    })
+
+    //返回一个对象
+    return {
+      name:toRef(person,'name'),
+      age:toRef(person,'age'),
+      cl:toRef(person.school,'cl')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
